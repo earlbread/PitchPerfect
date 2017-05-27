56ecff0ecaae7b2140f53a7cf8d422c198ea2b01
Seunghun Lee <waydi1@gmail.com>
2017-05-28 02:46:19 +0900

    Enable and disable the buttons mutually exclusively

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -11,10 +11,18 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear called")
+        stopRecordingButton.isEnabled = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,11 +33,15 @@ class ViewController: UIViewController {
     @IBAction func recordAudio(_ sender: Any) {
         print("Record button was pressed")
         recordingLabel.text = "Recording in Progress"
+        recordButton.isEnabled = false
+        stopRecordingButton.isEnabled = true
     }
 
     @IBAction func stopRecording(_ sender: Any) {
         print("Stop Recording button was pressed")
         recordingLabel.text = "Tap to Record"
+        recordButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
     }
 }
 
